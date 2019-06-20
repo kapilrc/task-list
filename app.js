c02256c7efaac7c5de05efca10a74f6da285b634
= <=>
2019-06-20 19:35:49 +0530

    added removeTask, clearTasks and filterTasks

--- app.js
+++ app.js
@@ -11,9 +11,18 @@ loadEventListeners();
 
 // load all event listeners
 function loadEventListeners() {
-  // Add task event
 
+  // Add task event
   form.addEventListener("submit", addTask);
+
+  // remove a task
+  taskList.addEventListener("click", removeTask);
+
+  // clear tasks
+  clearBtn.addEventListener("click", clearTasks);
+
+  // filter tasks
+  filter.addEventListener("keyup", filterTasks);
 }
 
 // Add task
@@ -51,4 +60,40 @@ function addTask(e) {
   taskInput.value = "";
 
   e.preventDefault();
+}
+
+// remove task
+function removeTask(e) {
+  if(e.target.parentElement.classList.contains("delete-item")) {
+    if(confirm("Are you sure?")){
+      e.target.parentElement.parentElement.remove();
+    }
+    
+  }
+}
+
+function clearTasks(e) {
+  // taskList.innerHTML = "";
+
+  // faster
+  while(taskList.firstChild){
+    taskList.removeChild(taskList.firstChild);
+  }
+}
+
+function filterTasks(e) {
+  const text = e.target.value.toLowerCase();
+  // console.log(text);
+
+  document.querySelectorAll(".collection-item").forEach(
+    function(task) {
+      const item = task.firstChild.textContent;
+
+      if(item.toLowerCase().indexOf(text) != -1) {
+        task.style.display = "block";
+      }else {
+        task.style.display = "none";
+      }
+    }
+  )
 }
